bb55e0e152ce2202c751ba9b8eb6eee17f823dd9
Farbkecks <fabian.alich@gmx.de>
2022-08-29 14:05:34 +0200

    add checkforWin

--- src/main/java/com/farbkecks/Board.java
+++ src/main/java/com/farbkecks/Board.java
@@ -11,6 +11,27 @@ public class Board {
         Arrays.fill(board, Player.NULL);
     }
 
+    boolean checkForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0 + i] == board[3 + i] && board[3 + i] == board[6 + i]) {
+                return true;
+            }
+        }
+        for (int i = 0; i < 7; i += 3) {
+            if (board[0 + i] == board[1 + i] && board[1 + i] == board[2 + i]) {
+                return true;
+            }
+        }
+        if (board[0] == board[4] && board[4] == board[8]) {
+            return true;
+        }
+        if (board[6] == board[4] && board[4] == board[2]) {
+            return true;
+        }
+
+        return false;
+    }
+
     private static char PlayerToChar(Player player) {
         switch (player) {
             case X:
