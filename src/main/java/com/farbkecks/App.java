bcd22bf8f9e72c6a7dca64fa1b3410912e19b429
Farbkecks <fabian.alich@gmx.de>
2022-09-09 12:12:37 +0200

    change gameloop

--- src/main/java/com/farbkecks/App.java
+++ src/main/java/com/farbkecks/App.java
@@ -13,11 +13,40 @@ public class App {
         }
     }
 
+    private static void gameloopOnePlayer(Board board, char player) {
+        while (true) {
+            board.show();
+            board.insert(player);
+            if (board.checkForEve() || board.checkForWin()) {
+                break;
+            }
+            player = changePlayer(player);
+            var index = MinMax.startMinMax(board.list, player);
+            board.insert(index, player);
+            if (board.checkForEve() || board.checkForWin()) {
+                break;
+            }
+            player = changePlayer(player);
+        }
+    }
+
+    private static void gameloopTwoPlayer(Board board, char player) {
+        while (true) {
+            board.show();
+            board.insert(player);
+            if (board.checkForEve() || board.checkForWin()) {
+                break;
+            }
+            player = changePlayer(player);
+        }
+    }
+
     public static void main(String[] args) {
         var towPlayer = false; // change for two or one player
         var color = true; // show board with color
         // doesn't work with some terminals
 
+        // get the info from the args
         for (String i : args) {
             if (i.equals("color")) {
                 color = false;
@@ -25,32 +54,21 @@ public class App {
                 towPlayer = true;
             } else {
                 System.out.println("color - disables the color on the board");
-                System.out.println("tow - enables the input from two players");
+                System.out.println("two - enables the input from two players");
                 System.out.println();
             }
         }
 
         var board = new Board(color);
         var player = 'X';
-        var computerPlaysNot = true;
-        while (board.checkForEve() == false && board.checkForWin() == false) { // ends gameloop when even or a win
-            // gameloop if the player plays
-            if (computerPlaysNot) {
-                board.show();
-                board.insert(player);
-                player = changePlayer(player);
-                if (towPlayer == false) {
-                    computerPlaysNot = false;
-                }
-            } else { // gameloop if the pc plays
-                var index = MinMax.startMinMax(board.list, player, color);
-                board.insert(index, player);
-                player = changePlayer(player);
-                computerPlaysNot = true;
-            }
+
+        if (towPlayer) {
+            gameloopTwoPlayer(board, player);
+        } else {
+            gameloopOnePlayer(board, player);
         }
-        board.show();
         player = changePlayer(player);
+        board.show();
         if (board.checkForWin()) {
             System.out.println(player + " hatt gewonne");
         } else {
